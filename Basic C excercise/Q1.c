115f0fc544db1987bc4cfb2a98b13663c3b6a26e
AlsoSanidhya <waynesfr@gmail.com>
2026-09-14 23:34:17 +0530

    second-largest distinct element

---
+++ Basic C excercise/Q1.c
@@ -0,0 +1,48 @@
+//Write a C program to find the second-largest distinct element in an array without sorting it.
+
+#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    int arr[100];
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    if (secondLargest == INT_MIN)
+    {
+        printf("No second-largest distinct element exists.\n");
+    }
+    else
+    {
+        printf("Second-largest distinct element = %d\n", secondLargest);
+    }
+
+    return 0;
+}
